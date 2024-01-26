a2e0b7dca8b64a41b63509ff0abaf6e82302135e
Ben gardiner <gardinbe@gmail.com>
2024-01-26 13:06:55 +0000

    update middlewares

--- api/config/middlewares.ts
+++ api/config/middlewares.ts
@@ -8,6 +8,8 @@ export default [
 			origin: [
 				"http://localhost:1337",
 				"http://127.0.0.1:1337",
+				"http://localhost:5173",
+				"http://127.0.0.1:5173",
 				"http://joshuacurtis.co.uk",
 				"https://joshuacurtis.co.uk",
 				"http://www.joshuacurtis.co.uk",
